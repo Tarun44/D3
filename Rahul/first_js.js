6c9f032dca05d5b7604f02f05699896f8b023331
nigamrahul <nigamrahul1992@yahoo.com>
2017-01-13 17:29:33 +0530

    Taking Overview of Javascript with ex

---
+++ Rahul/first_js.js
@@ -0,0 +1,24 @@
+function me(){
+		var x=10;
+		var y=12;
+		var z=alert(x+y);
+}
+
+function show(name,age){
+			document.write("Name of The Candidate is " + name +" & age is " +age );
+}
+
+function full(first,last){
+		document.write(first + last+" is the complete name of the candidate!")
+}
+function change(x){
+   x.style.color="red";
+   x.style.background="aquamarine";
+}
+
+function out(x){
+	x.style.color="black";
+	x.style.background="transparent";
+	// x.style.;
+}
+
