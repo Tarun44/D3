3aee55740111efa64ce008ab59f30cce1178d16c
shalini852 <shalinidutta16@gmail.com>
2017-01-19 18:54:12 +0530

    updated

--- Shalini/script.js
+++ Shalini/script.js
@@ -1,8 +1,22 @@
-$(document).ready(function(){
-	$("#selectField").on("change", function() {
-		console.log($(this).val(),$(this))
-		var selected= $(this);
-		if (selected != "All") {
+$(document).ready(function() {
+
+    function addRemoveClass(theRows) {
+
+        theRows.removeClass("odd even");
+        theRows.filter(":odd").addClass("odd");
+        theRows.filter(":even").addClass("even");
+    }
+
+    var rows = $("table#myTable tr:not(:first-child)");
+
+    addRemoveClass(rows);
+
+
+    $("#selectField").on("change", function() {
+
+        var selected = this.value;
+
+        if (selected != "All") {
 
             rows.filter("[position=" + selected + "]").show();
             rows.not("[position=" + selected + "]").hide();
@@ -12,7 +26,8 @@ $(document).ready(function(){
 
             rows.show();
             addRemoveClass(rows);
+
         }
 
-	})
+    });
 });
