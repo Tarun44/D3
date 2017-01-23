342ab75dd0dfce91292b3808b3db2a9d438854d4
shalini852 <shalinidutta16@gmail.com>
2017-01-23 10:08:09 +0530

    updated

---
+++ Shalini/my_script1.js
@@ -0,0 +1,6 @@
+$(document).ready( function() {
+ $.getJSON("json_data.json", function(data){
+       $.each(data.person, function(){
+	         $("ul").append("<li>Name: "+this['name']+"</li><li>Age: "+this['name']+"</li>");
+	   });
+ });
---
+++ Shalini/script1.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('#click me').click(function() {
+		$.getJSON('json_data.json', function(data) {
+			var items = [];
+			$.each(data, function(key, val) {
+				items.push('<li id="' + key + '">' + val + '</li>');
+			});
+			$('<ul/>', {
+				'class': 'interest-list',
+				html: items.join('')
+			}).appendTo('body');
+			
+		});
+	});
+});
